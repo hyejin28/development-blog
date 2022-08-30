c2c00d04e8710fc5ebb59c0dbfb9eedc9dfcda1a
정혜진 <wjdgpwls0528@naver.com>
2022-08-30 14:36:51 +0900

    add

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,26 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ getConfig, actions}) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'), //절대 경로를 사용할 수 있게 해줌
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
   })
 }
