1f7f1a54bc3ba3a18b5f6ad7597758e8d5f298ac
정혜진 <wjdgpwls0528@naver.com>
2022-12-17 03:04:50 +0900

    develop main

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,25 @@
+//페이지 하단 푸터
+
+import React, { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+
+const FooterWrapper = styled.div`
+    display: grid;
+    place-items: center;
+    margin-top: auto;
+    padding: 50px 0;
+    font-size: 15px;
+    text-align: center;
+    line-height: 1.5;
+`
+
+const Footer: FunctionComponent = function() {
+    return(
+        <FooterWrapper>
+            Thank You for Visiting My Blog, Have a good Day
+            <br /> 2022 Developer Jin, Powerd By Gatsby.
+        </FooterWrapper>
+    )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import React, { FunctionComponent } from 'react'
-import Text from '../components/Text'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import Introduction from 'components/Main/Introduction'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />
+  return (
+    <Container>
+        <GlobalStyle />
+        <Introduction />
+        <Footer/>
+    </Container>
+  )
 }
 
 export default IndexPage
