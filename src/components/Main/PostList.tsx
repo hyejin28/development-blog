625804b4723cb72072a5d9cedab479aba2500b03
정혜진 <wjdgpwls0528@naver.com>
2022-12-22 01:16:27 +0900

    props post data

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -3,17 +3,37 @@
 import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
 import PostItem from "./PostItem";
+import { PostListItemType } from 'types/PostItem.types'
 
-const POST_ITEM_DATA = {
-    title: 'Post Item Title',
-    date: '2022.12.20.',
-    categories: ['Web', 'Fronted', 'Testing'],
-    summary:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-    thumbnail:
-        'http://www.koreaisacademy.com/renewal2016/img/curriculum/web02.jpg',
-    link: 'https://www.google.co.kr/'
+export type PostType = {
+    node: {
+        id: string
+        frontmatter: {
+            title: string
+            summary: string
+            date: string
+            categories: string[]
+            thumbnail: {
+                publicURL: string
+            }
+        }
+    }
 }
+
+type PostListProps = {
+    posts: PostListItemType []
+}
+
+// const POST_ITEM_DATA = {
+//     title: 'Post Item Title',
+//     date: '2022.12.20.',
+//     categories: ['Web', 'Fronted', 'Testing'],
+//     summary:
+//         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+//     thumbnail:
+//         'http://www.koreaisacademy.com/renewal2016/img/curriculum/web02.jpg',
+//     link: 'https://www.google.co.kr/'
+// }
 
 const PostListWrapper = styled.div`
     display: grid;
@@ -30,13 +50,20 @@ const PostListWrapper = styled.div`
     }
 `
 
-const PostList: FunctionComponent = function () {
+const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
     return (
     <PostListWrapper>
-        <PostItem {...POST_ITEM_DATA}/>
-        <PostItem {...POST_ITEM_DATA}/>
-        <PostItem {...POST_ITEM_DATA}/>
-        <PostItem {...POST_ITEM_DATA}/>
+        {posts.map(
+            ({
+             node: { id, frontmatter },
+             }: PostType) => (
+                <PostItem
+                    {...frontmatter}
+                    link="https://www.google.co.kr/"
+                    key={id}
+                />
+            ),
+        )}
     </PostListWrapper>
     )
 }
